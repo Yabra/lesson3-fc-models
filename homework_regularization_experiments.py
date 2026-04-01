780f27477c6c942b46072f5389931e8b8e4a8bcf
yabra <35022445+Yabra@users.noreply.github.com>
2026-04-01 11:17:12 +0500

    3.2

--- homework_regularization_experiments.py
+++ homework_regularization_experiments.py
@@ -7,8 +7,19 @@ from fully_connected_basics.datasets import get_mnist_loaders, get_cifar_loaders
 from fully_connected_basics.models import FullyConnectedModel
 from fully_connected_basics.utils import plot_training_history, count_parameters
 
+def set_dropout(model, rate):
+    for m in model.modules():
+        if isinstance(m, nn.Dropout):
+            m.p = rate
+
+def set_batchnorm_momentum(model, momentum):
+    for m in model.modules():
+        if isinstance(m, nn.BatchNorm1d):
+            m.momentum = momentum
+
 # слегка изменим train_model, чтобы оптимизатору можно было задать параметр weight_decay
-def train_model(model, train_loader, test_loader, epochs=10, lr=0.001, device='cpu', weight_decay=None):
+# а также можно было применить адаптивные техники Dropout и BatchNorm
+def train_model(model, train_loader, test_loader, epochs=10, lr=0.001, device='cpu', weight_decay=None, dynamic_dropout=False, dynamic_batchnorm=False):
     criterion = nn.CrossEntropyLoss()
 
     if weight_decay is None:
@@ -20,6 +31,16 @@ def train_model(model, train_loader, test_loader, epochs=10, lr=0.001, device='c
     test_losses, test_accs = [], []
     
     for epoch in range(epochs):
+        # добавляем изменения Dropout при смене эпох
+        if dynamic_dropout:
+            current_dropout = 0.5 * (1 - epoch / epochs)
+            set_dropout(model, current_dropout)
+
+        # добавляем изменения BatchNorm при смене эпох
+        if dynamic_batchnorm:
+            current_momentum = 0.1 * 0.5 * (epoch / epochs)
+            set_batchnorm_momentum(model, current_momentum)
+
         train_loss, train_acc = run_epoch(model, train_loader, criterion, optimizer, device, is_test=False)
         test_loss, test_acc = run_epoch(model, test_loader, criterion, None, device, is_test=True)
         
@@ -47,62 +68,79 @@ train_loader, test_loader = get_mnist_loaders()
 
 
 # слои для 3.1
-depth_configs = {
-    "L2": [
-        {"type": "linear", "size": 256},
-        {"type": "linear", "size": 128},
-        {"type": "linear", "size": 64}
-    ],
+# depth_configs = {
+#     "L2": [
+#         {"type": "linear", "size": 256},
+#         {"type": "linear", "size": 128},
+#         {"type": "linear", "size": 64}
+#     ],
     
-    "Без регуляризации": [
-            {"type": "linear", "size": 256},
-            {"type": "linear", "size": 128},
-            {"type": "linear", "size": 64}
-        ],
+#     "Без регуляризации": [
+#             {"type": "linear", "size": 256},
+#             {"type": "linear", "size": 128},
+#             {"type": "linear", "size": 64}
+#         ],
 
-    "Dropout 0.1": [
-            {"type": "linear", "size": 256},
-            {"type": "dropout", "rate": 0.1},
-            {"type": "linear", "size": 128},
-            {"type": "dropout", "rate": 0.1},
-            {"type": "linear", "size": 64}
-        ],
+#     "Dropout 0.1": [
+#             {"type": "linear", "size": 256},
+#             {"type": "dropout", "rate": 0.1},
+#             {"type": "linear", "size": 128},
+#             {"type": "dropout", "rate": 0.1},
+#             {"type": "linear", "size": 64}
+#         ],
 
-    "Dropout 0.3": [
-            {"type": "linear", "size": 256},
-            {"type": "dropout", "rate": 0.3},
-            {"type": "linear", "size": 128},
-            {"type": "dropout", "rate": 0.3},
-            {"type": "linear", "size": 64}
-        ],
+#     "Dropout 0.3": [
+#             {"type": "linear", "size": 256},
+#             {"type": "dropout", "rate": 0.3},
+#             {"type": "linear", "size": 128},
+#             {"type": "dropout", "rate": 0.3},
+#             {"type": "linear", "size": 64}
+#         ],
 
-    "Dropout 0.5": [
-            {"type": "linear", "size": 256},
-            {"type": "dropout", "rate": 0.5},
-            {"type": "linear", "size": 128},
-            {"type": "dropout", "rate": 0.5},
-            {"type": "linear", "size": 64}
-        ],
+#     "Dropout 0.5": [
+#             {"type": "linear", "size": 256},
+#             {"type": "dropout", "rate": 0.5},
+#             {"type": "linear", "size": 128},
+#             {"type": "dropout", "rate": 0.5},
+#             {"type": "linear", "size": 64}
+#         ],
 
-    "BatchNorm": [
-            {"type": "linear", "size": 256},
-            {"type": "batch_norm"},
-            {"type": "linear", "size": 128},
-            {"type": "batch_norm"},
-            {"type": "linear", "size": 64}
-        ],
+#     "BatchNorm": [
+#             {"type": "linear", "size": 256},
+#             {"type": "batch_norm"},
+#             {"type": "linear", "size": 128},
+#             {"type": "batch_norm"},
+#             {"type": "linear", "size": 64}
+#         ],
 
-    "BatchNorm + Dropout": [
-            {"type": "linear", "size": 256},
-            {"type": "batch_norm"},
-            {"type": "dropout", "rate": 0.3},
-            {"type": "linear", "size": 128},
-            {"type": "batch_norm"},
-            {"type": "dropout", "rate": 0.3},
-            {"type": "linear", "size": 64}
-        ]
+#     "BatchNorm + Dropout": [
+#             {"type": "linear", "size": 256},
+#             {"type": "batch_norm"},
+#             {"type": "dropout", "rate": 0.3},
+#             {"type": "linear", "size": 128},
+#             {"type": "batch_norm"},
+#             {"type": "dropout", "rate": 0.3},
+#             {"type": "linear", "size": 64}
+#         ]
+# }
+
+depth_configs = {
+    "Adaptive": [
+        {"type": "linear", "size": 256},
+        {"type": "batch_norm"},
+        {"type": "relu"},
+        {"type": "dropout", "rate": 0.5},
+
+        {"type": "linear", "size": 128},
+        {"type": "batch_norm"},
+        {"type": "relu"},
+        {"type": "dropout", "rate": 0.5},
+
+        {"type": "linear", "size": 64},
+        {"type": "batch_norm"},
+        {"type": "relu"}
+    ],
 }
-
 
 all_historyes = []
 
@@ -123,7 +161,10 @@ for name, layers in depth_configs.items():
         epochs=5,
         lr=0.001,
         device="cuda",
-        weight_decay=(1e-4 if name == "L2" else None))
+        weight_decay=(1e-4 if name == "L2" else None),
+        dynamic_dropout=name == "Adaptive",
+        dynamic_batchnorm=name == "Adaptive",
+        )
     
     all_historyes.append([history, name, model])
 
@@ -143,8 +184,11 @@ for history, label, model in all_historyes:
     
 
 
-ax1.set_title('Accuracy')
+ax1.set_title("Accuracy")
 ax1.legend()
+
+ax2.set_title("Weight distribution")
+ax2.legend()
 
 plt.tight_layout()
 
